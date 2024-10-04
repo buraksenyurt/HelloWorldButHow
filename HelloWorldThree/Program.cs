34a0ef6545aa2c59a0528967c7c498bc579dc34d
buraksenyurt <burakselimsenyurt@gmail.com>
2024-10-04 17:38:11 +0300

    Added Hello World III codes

---
+++ HelloWorldThree/Program.cs
@@ -0,0 +1,113 @@
+﻿using Model;
+
+var gameWorld = new World();
+
+var legolasComponents = new List<object>
+{
+    new Position(200,100),
+    new Health(50),
+    new Power(10)
+};
+
+var legolas = gameWorld.AddEntity(legolasComponents);
+var tower1 = gameWorld.AddEntity(new List<object>
+{
+    new Position(100,0),
+    new Health(200)
+});
+var tower2 = gameWorld.AddEntity(new List<object>
+{
+    new Position(0,0),
+    new Health(200)
+});
+
+foreach (var kv in gameWorld.Entities)
+{
+    Console.WriteLine(kv.Key);
+    foreach (var entity in kv.Value)
+    {
+        Console.WriteLine("\t{0}", entity.ToString());
+    }
+}
+
+namespace Model
+{
+    public class World
+    {
+        private readonly Dictionary<Guid, List<object>> entitites = [];
+
+        public Guid AddEntity(List<object> objects)
+        {
+            var id = Guid.NewGuid();
+            entitites.Add(id, objects);
+            return id;
+        }
+
+        public List<object> GetEntity(Guid id)
+        {
+            return entitites[id];
+        }
+
+        public Dictionary<Guid, List<object>> Entities
+        {
+            get { return entitites; }
+        }
+
+    }
+    public class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return $"Pos ({X}:{Y})";
+        }
+    }
+
+    public class Health
+    {
+        public int Value { get; private set; } = 100;
+
+        public Health(int value)
+        {
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return $"Health ({Value})";
+        }
+    }
+
+    public class Velocity
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Velocity(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return $"Vel ({X}:{Y})";
+        }
+    }
+
+    public class Power
+    {
+        public int Value { get; private set; }
+        public Power(int value)
+        {
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return $"Power ({Value})";
+        }
+    }
+}
